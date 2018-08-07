81b480bc183b2d0f35694705aa16fcbacc6dcaea
Kirill Karmadonov <kirill@live.com>
2018-08-07 09:47:07 +0300

    Added login functionality

--- sources/core/settings.py
+++ sources/core/settings.py
@@ -54,8 +54,9 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 ],
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -118,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGOUT_REDIRECT_URL = '/'
--- sources/core/urls.py
+++ sources/core/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(
+        'login/',
+        auth_views.LoginView.as_view(extra_context={'next': '/login'}),
+        name='login'
+    ),
+    path('logout/', auth_views.LogoutView.as_view()),
+    path('change-password/', auth_views.PasswordChangeView.as_view()),
 ]
